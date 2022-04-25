664f0251ab6390437e9d850965d3fe3893a1810d
Bolanle <bola0321@gmail.com>
2022-04-25 10:59:23 -0400

    turned word into ArrayList to make method easier

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,12 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class PigLatin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Scanner scnr = new Scanner(System.in);
+		//System.out.print("Please Enter a word to translate into Pig Latin: ");
+		//String pigLatin = scnr.nextLine().toLowerCase();
+		
+		String pigLatin = "anger";
+		System.out.println(translate(pigLatin));
 
 	}
 
-	public static String translate(String word) {
+	public static List<Character> translate(String word) {
+		char[] wordToArray = word.toCharArray();
 		
+		List<Character> arrToArrayList = new ArrayList <Character>();
+		for(char text : wordToArray) {
+			arrToArrayList.add(text);
+		}
+		return arrToArrayList;
+		//if ()
 	}
 }
